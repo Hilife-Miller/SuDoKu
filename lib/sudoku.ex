1c31b5b5e1ffe1cf0aad3ab24e5c70a469ccc724
Richard Miller <richardmillerii@Richards-MacBook-Pro.local>
2018-04-14 20:51:25 -0400

    Added col, cols, rows, sq_coordinate, square

--- lib/sudoku.ex
+++ lib/sudoku.ex
@@ -23,4 +23,27 @@ defmodule Sudoku do
       [4,1,2,3]
     ]
   end
+  def rows(board) do
+    board
+  end
+  def col(board,index) do
+    Enum.map(board, &Enum.at( &1, index))
+  end
+  def cols(board) do
+    Enum.map((0..3), &(col(board, &1)))
+  end
+  def sq_coordinate(index) do
+    {div(index, 2), rem(index, 2)}
+  end
+  def square(board, {r,c}) do
+    board
+    |> Enum.drop(r*2)
+    |> Enum.take(2)
+    |> Enum.map( & (&1 |> Enum.drop(c*2) |> Enum.take(2)))
+    |> List.flatten
+  end
+  def square(board, index) do
+    square(board, sq_coordinate(index))
+  end
+
 end
